f3ad40aebf2b0e8d3e2b18c1a32252b0a4929a6a
FloydWatson <floydbcseit@gmail.com>
2020-06-20 18:36:24 +1200

    added animated container

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -254,19 +254,17 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        // builder rebuiilds this child
-        builder: (ctx, ch) => Container(
-          // old way of adjusting size
-            // height: _authMode == AuthMode.Signup ? 320 : 260,
-            height: _heightAnimation.value.height,
-            constraints:
-                BoxConstraints(minHeight: _heightAnimation.value.height),
-            width: deviceSize.width * 0.75,
-            padding: EdgeInsets.all(16.0),
-            child: ch),
-            // this child will not be rebuilt every animation tick
+      // animated container handles container animations. allows you to not have to build animations. doesnt need controller. handles everything for you
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // for using animation builder
+        // height: _heightAnimation.value.height,
+        constraints:
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
